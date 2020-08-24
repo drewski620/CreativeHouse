b1b38ffa851a144cdcd313d5fb11117acf98bd1b
Andrew Werner <drewski620@gmail.com>
2020-08-24 15:34:13 -0600

    Layout of homepage

---
+++ script.js
@@ -0,0 +1,20 @@
+ $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('white');
+                  $('nav ul li a').addClass('black');
+                  $('.logo').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('white');
+                  $('nav ul li a').removeClass('black');
+            }
+      })
